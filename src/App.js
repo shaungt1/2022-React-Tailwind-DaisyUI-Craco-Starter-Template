e2837bc1357ac46d1e5706328c09a17609cdfd13
Shaun Pritchard <shaunpritchard1@gmail.com>
2022-02-02 17:21:26 -0500

    init commit
    pushed files intimal commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import img from "./react.png";
+
+function App() {
+  const h1 = " React";
+  const newLine = <br />;
+
+  return (
+    <div className="  flex flex-row min-h-screen justify-center items-center bg-purple-500 ">
+      <div className=" text-center">
+        <img className="loaderIcon" src={img} alt="react tailwind daisyui" />
+      </div>
+      <div className="flex flex-col ">
+        <h1 className="text-4xl text-center">{h1.toUpperCase()}</h1>
+        <h3 className="text-lg text-center">Tailwind DaisUI Starter</h3>
+        <p>{newLine}</p>
+        <div className="flex flex-row justify-center">
+          <a href="https://daisyui.com/">
+            <button className="my-3 mx-2 flex  flex-col justify-center items-center btn ">
+              Tailwind Docs
+            </button>
+          </a>
+          <a href="https://daisyui.com/">
+            <button className="my-3 mx-2 flex flex-col justify-center items-center btn ">
+              Daisy UI Docs
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
